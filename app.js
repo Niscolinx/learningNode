ae595c661dafdefc7b8aabf5e420727696cffbc8
Niscolinx <munisco12@gmail.com>
2020-06-13 20:11:41 +0100

    structured my files and used express router for easier navigation

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const adminRoute = require('./routes/admin')
+const shopRoute = require('./routes/shop')
+
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next) => {
-    console.log('Adding products')
-   // res.write('<h3>This is the product page, so please add your product below</h3>')
-    res.send('<form action="/product" method="POST"><input type="text" placeholder="fill in your book title" name="bookTitle"></input><button>Add Product</button></form>')
-})
-
-app.post('/product', (req, res, next) => {
-    const {bookTitle} = req.body
-    console.log('This is the book title', bookTitle)
-    res.send('Book title added')
-})
-
-app.use('/', (req, res, next) => {
-    console.log('The home page')
-    res.send('<h3>This is the home page</h3>')
-})
+app.use(adminRoute)
+app.use(shopRoute)
 
 app.listen(3030, () => {
     console.log('listening to port', 3030)
