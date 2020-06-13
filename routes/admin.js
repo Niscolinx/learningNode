0bbf069e5813ba21a50dff236cd113e79a606d49
Niscolinx <munisco12@gmail.com>
2020-06-13 21:47:03 +0100

    Added html files

--- routes/admin.js
+++ routes/admin.js
@@ -5,10 +5,10 @@ const router = express.Router()
 router.get('/add-product', (req, res, next) => {
     console.log('Adding products')
     // res.write('<h3>This is the product page, so please add your product below</h3>')
-    res.send('<form action="/product" method="POST"><input type="text" placeholder="fill in your book title" name="bookTitle"></input><button>Add Product</button></form>')
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" placeholder="fill in your book title" name="bookTitle"></input><button>Add Product</button></form>')
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     const { bookTitle } = req.body
     console.log('This is the book title', bookTitle)
     res.send('Book title added')
