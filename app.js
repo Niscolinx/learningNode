89f71fadd940d0059b1d11acf7e6988703ae360b
Niscolinx <munisco12@gmail.com>
2020-06-13 18:42:06 +0100

    working with an express server

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/add-product', (req, res, next) => {
     console.log('Adding products')
-    res.send('<h3>This is the product page, so please add your product below</h3>')
-    res.send('<form action="/product" method="POST"><input type="text" placeholder="fill in your book title" name="book-title"></input><button>Add Product</button></form>')
+   // res.write('<h3>This is the product page, so please add your product below</h3>')
+    res.send('<form action="/product" method="POST"><input type="text" placeholder="fill in your book title" name="bookTitle"></input><button>Add Product</button></form>')
 })
 
 app.post('/product', (req, res, next) => {
-    const {name} = req.body
-    console.log('This is the book title', name)
+    const {bookTitle} = req.body
+    console.log('This is the book title', bookTitle)
+    res.send('Book title added')
 })
 
 app.use('/', (req, res, next) => {
@@ -21,5 +22,5 @@ app.use('/', (req, res, next) => {
 })
 
 app.listen(3030, () => {
-    console.log('listening to port', port)
+    console.log('listening to port', 3030)
 })
