aedb757a3795f79de137ee2f519c855dc6d41d38
Niscolinx <munisco12@gmail.com>
2020-06-13 20:44:10 +0100

    structured my files and used express router for easier navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,17 @@
 const express = require('express')
 
-const adminRoute = express.Router()
+const router = express.Router()
 
-adminRoute.use('/add-product', (req, res, next) => {
+router.use('/add-product', (req, res, next) => {
     console.log('Adding products')
     // res.write('<h3>This is the product page, so please add your product below</h3>')
     res.send('<form action="/product" method="POST"><input type="text" placeholder="fill in your book title" name="bookTitle"></input><button>Add Product</button></form>')
 })
 
-adminRoute.post('/product', (req, res, next) => {
+router.post('/product', (req, res, next) => {
     const { bookTitle } = req.body
     console.log('This is the book title', bookTitle)
     res.send('Book title added')
 })
 
-module.exports = adminRoute;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
-const shopRoute = express.Router()
+const router = express.Router()
 
 
-shopRoute.use('/', (req, res, next) => {
+router.use('/', (req, res, next) => {
     console.log('The home page')
     res.send('<h3>This is the home page</h3>')
 })
 
-module.exports = shopRoute;
+module.exports = router;
