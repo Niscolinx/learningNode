2d4f59f923ead41d10c21b364075aed8fa7ee1a6
Niscolinx <munisco12@gmail.com>
2020-06-13 18:22:29 +0100

    setting up an express server

--- app.js
+++ app.js
@@ -6,5 +6,12 @@ app.use(bodyParser.urlencoded())
 
 app.use('/add-product', (req, res, next) => {
     console.log('Adding products')
-    
+    res.send('<h3>This is the product page, so please add your product below</h3>')
+})
+
+app.
+
+app.use('/', (req, res, next) => {
+    console.log('The home page')
+    res.send('<h3>This is the home page</h3>')
 })
