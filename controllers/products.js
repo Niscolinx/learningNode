84f8c007b1dfdc36f313ad0810d50a64512ab5fe
Niscolinx <munisco12@gmail.com>
2020-06-28 22:02:15 +0100

    Adding some styling

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Products = require('../models/products')
 
 exports.getAddProducts = (req, res, next) => {
 
-    res.render('admin/add-product', { pageTitle: 'Add product' })
+    res.render('admin/add-product', { pageTitle: 'Add product', path: '/admin/add-product' })
 }
 
 exports.postAddProducts = (req, res, next) => {
@@ -15,6 +15,6 @@ exports.postAddProducts = (req, res, next) => {
 
 exports.getShop = (req, res, next) => {
     Products.fetchAll(products => {
-        res.render('shop/list-products', { products, pageTitle: 'My shop' })
+        res.render('shop/list-products', { products, pageTitle: 'My shop', path: '/'})
     })
 }
