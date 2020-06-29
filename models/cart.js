d4caff411df4ed8b1b8b4dee2e1dadf954972b07
Niscolinx <munisco12@gmail.com>
2020-06-29 08:00:37 +0100

    building the remove cart function

--- models/cart.js
+++ models/cart.js
@@ -26,14 +26,25 @@ module.exports = class Cart {
         getItemsFromCart(cart => {
             cart.push(this)
             console.log('This is the cart item', cart)
-            fs.writeFile(cartPath, JSON.stringify(cart), (er, file) => {
-                console.log('This is the error', er, file)
+            fs.writeFile(cartPath, JSON.stringify(cart), err => {
+                console.log('The error from saving the cart item',err)
             })
 
         })
 
     }
 
+    remove(id){
+        getItemsFromCart(cart => {
+           const newItems =  cart.filter(cartItem => {
+                cartItem.id !== id
+            })
+            cart = newItems
+            fs.writeFile(cartPath, JSON.stringify(cart), err => {
+                console.log('The error from removing the cart', err)
+            })
+        })
+    }
 
     static fetchAll(cb) {
         getItemsFromCart(cb)
