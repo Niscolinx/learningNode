b7249f3e2c58450627502ce82b984e5f0580a740
Niscolinx <munisco12@gmail.com>
2020-07-01 07:57:17 +0100

    refactoring

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,11 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../controllers/admin')
-const shopController = require('../controllers/shop')
 
 router.get('/add-product', adminController.getAddProducts)
 
 router.post('/add-product', adminController.postAddProducts)
 
-router.post('/remove-product', shopController.postRemoveProduct)
+router.post('/remove-product', adminController.postRemoveProduct)
 
 module.exports = router
