0c9aa2abbe3d4862d0e930b9c0fb55cf0ef09269
Niscolinx <munisco12@gmail.com>
2020-07-03 07:11:11 +0100

    Calculating the totalprice

--- models/getTotalPrice.js
+++ models/getTotalPrice.js
@@ -2,19 +2,21 @@
 const fs = require('fs')
 const path = require('path')
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'cart.json')
+const tp = path.join(path.dirname(process.mainModule.filename), 'data', 'totalprice.json')
+const cp = path.join(path.dirname(process.mainModule.filename), 'data', 'cart.json')
 
 
-const getTotalPrice = () => {
-    fs.readFile(p, (err, fileContents) => {
+
+const getTotalPrice = fs.readFile(cp, (err, fileContents) => {
         if (err) {
-            return []
+           // res.send('Hello world')
+           console.log('the error', err)
         }
         else {
             return JSON.parse(fileContents)
         }
     })
-}
+
 // getItemsFromCart(cartItem => {
 //     let total = 0
 //     for (let item of cartItem) {
