7432833c18543ebf00bf8fc3a7a7dcc0d511c5e8
Igboanugwo Collins <munisco12@gmail.com>
2020-07-09 10:47:15 +0100

    Created the user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 
 const sequelize = require('../util/database')
 
-const Product = sequelize.define('product', {
+const User = sequelize.define('user', {
     id: {
         type: Sequelize.INTEGER,
         allowNull: false,
