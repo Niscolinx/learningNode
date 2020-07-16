dfae23285b76cb1d4afb399e851a881454fa480a
Igboanugwo Collins <munisco12@gmail.com>
2020-07-16 15:37:15 +0100

    Mongodb connected successfully, wow!!

--- util/database.js
+++ util/database.js
@@ -3,40 +3,18 @@ const MongoClient = require('mongodb').MongoClient;
 
 
 let _db;
-//const uri = "mongodb+srv://cluster0.zhgsa.mongodb.net/learningNode"
-//const uri = "mongodb+srv://munisco:munisco6869@cluster0.zhgsa.mongodb.net/products?//retryWrites=true&w=majority";
 
 const MongoConnect = cb => {
-    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
-    // client.connect(err => {
-        //     const collection = client.db("test").collection("devices")
-    //     _db = client.db("learningNode").collection("users")
-    //     cb(collection)
-    //     // perform actions on the collection object
-    //     //client.close();
-    // });
-    // MongoClient.connect(uri, (err, client) => {
-        //     if(err) {
-            //         console.log('err connecting', err)
-    //         return
-    //     }
     
-    //     const db = client.db('learningNode')
-    //     cb(client)
-    //     _db = db
-    
-    // })
-    
-    const uri = "mongodb://localhost:105.112.182.148/32";
+    const uri = 'mongodb+srv://munisco:fkNZcq4s9ZmcXho5@cluster0.zhgsa.mongodb.net/learningNode'
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-    client.connect(err => {
-       // const collection = client.db("test").collection("devices");
- 
-        // perform actions on the collection object
+    client.connect((err, db) => {
+        console.log('arguments from connect', err, db)
+      
         cb(client)
-        //client.close();
         _db = client.db('learningNode')
-    });
+    })
+
 
 }
 
