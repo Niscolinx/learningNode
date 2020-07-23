d6b50a52c97357dd052e9beedba01ba5441cf44c
Igboanugwo Collins <munisco12@gmail.com>
2020-07-23 14:05:43 +0100

    update

--- app.js
+++ app.js
@@ -20,23 +20,21 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-//const { MongoConnect } = require('./util/database');
 const mongoose = require('mongoose')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//5f14a399fe813c1948012cdf  the user id from the db
-// app.use((req, res, next) => {
-//     const newUser = new User('Collins', 'munisco12@gmail.com')
-//     newUser.save()
-//         .then(user => {
-//             req.user = user
-//             next()
-//         })
-//         .catch(err => console.log('user failure from db', err))
-// })
+//5f1886043ad46f4a976f4157 the user id from the db
+app.use((req, res, next) => {
+    User.findById('5f1886043ad46f4a976f4157')
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log('user failure from db', err))
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -47,6 +45,18 @@ app.use(errorController.get404);
 mongoose.connect('mongodb+srv://munisco:fkNZcq4s9ZmcXho5@cluster0.zhgsa.mongodb.net/shop', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(result => {
     console.log('connected to the client')
+
+    User.findOne().then(user => {
+        if(!user){
+            const user = new User({
+                name: 'Collins', email: 'munisco12@gmail.com', cart: {
+                    items: []
+                }
+            })
+            user.save()
+        }
+    })
+    
     app.listen(3030, () => {
         console.log('Listening on 3030')
     })
