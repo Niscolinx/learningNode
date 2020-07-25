3a823f926bdabe1cb5a3d196d7e4e2cf13b2d3f7
Igboanugwo Collins <munisco12@gmail.com>
2020-07-25 21:58:54 +0100

    Added the login ejs page

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 const mongoose = require('mongoose')
 
@@ -37,6 +38,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
