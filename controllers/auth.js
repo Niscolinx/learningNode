aefe642ebf642c431483b4669081ba9d3af2189e
Igboanugwo Collins <munisco12@gmail.com>
2020-08-11 15:12:23 +0100

    Added async validation for the signup email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs')
 const nodemailer = require('nodemailer')
 const crypto = require('crypto')
-const {validationResult} = require('express-validator')
+const { validationResult } = require('express-validator')
 
 const User = require('../models/user')
 
@@ -25,7 +25,11 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+    }
   });
 };
 
@@ -38,9 +42,20 @@ exports.postLogin = (req, res, next) => {
     return res.status(422).render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email,
+        password,
+      }
     });
   }
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        req.flash('message', 'User does not exist, please sign up')
+        return res.redirect('/signup')
+      }
+
       bcrypt.compare(password, user.password)
         .then(doesMatch => {
           if (doesMatch) {
@@ -56,6 +71,7 @@ exports.postLogin = (req, res, next) => {
             res.redirect('/login')
           }
         })
+    })
     .catch(err => {
       console.log('Failed to user in', err)
     })
@@ -81,55 +97,58 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
   });
 };
 
 exports.postSignup = (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password, confirmPassword } = req.body;
 
   const errors = validationResult(req)
-  if(!errors.isEmpty()){
+  if (!errors.isEmpty()) {
     console.log('the validation err', errors.array())
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email,
+        password,
+        confirmPassword
+      }
     });
   }
 
-  User.findOne({ email })
-    .then(user => {
-      if (user) {
-        req.flash('message', 'User already exits, please login!!')
-        return res.redirect('/login')
-      }
-      bcrypt.hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email,
-            password: hashedPassword,
-            cart: {
-              items: []
-            }
-          })
-          return user.save()
-        }).catch(err => { console.log(err) })
-
-      res.redirect('/login')
-      mailTransport.sendMail({
-        to: email,
-        from: 'munisco12@gmail.com',
-        subject: 'Successful sign up',
-        html: '<h1>Welcome, you have signed Up successfully!!</h1>'
-
+  bcrypt.hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email,
+        password: hashedPassword,
+        cart: {
+          items: []
+        }
       })
-        .then(result => {
-          console.log('from the mailer', result)
-        })
-        .catch(err => console.log(err))
+      return user.save()
+    }).catch(err => { console.log(err) })
 
-    })
+  res.redirect('/login')
+  mailTransport.sendMail({
+    to: email,
+    from: 'munisco12@gmail.com',
+    subject: 'Successful sign up',
+    html: '<h1>Welcome, you have signed Up successfully!!</h1>'
+
+      .then(result => {
+        console.log('from the mailer', result)
+      })
+      .catch(err => console.log(err))
+
+  })
     .catch(err => {
       console.log(err)
       res.redirect('/signup')
