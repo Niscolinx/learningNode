eb38fe8252ad1eccc293451a3f868e5c83187561
Igboanugwo Collins <munisco12@gmail.com>
2020-08-19 07:42:28 +0100

    Adding Error handlers so that the app will not crash

--- app.js
+++ app.js
@@ -1,71 +1,98 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const mongoDbSession = require('connect-mongodb-session')(session)
 const csrf = require('csurf')
 const flash = require('connect-flash')
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+const authRoutes = require('./routes/auth')
 
 const User = require('./models/user')
 
-const errorController = require('./controllers/error');
+const errorController = require('./controllers/error')
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-const MONGODB_URI = 'mongodb+srv://munisco:fkNZcq4s9ZmcXho5@cluster0.zhgsa.mongodb.net/shop'
+const MONGODB_URI =
+    'mongodb+srv://munisco:fkNZcq4s9ZmcXho5@cluster0.zhgsa.mongodb.net/shop'
 const csrfToken = csrf()
 
 const store = new mongoDbSession({
     uri: MONGODB_URI,
-    collection: 'sessions'
+    collection: 'sessions',
 })
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store,
+    })
+)
 app.use(csrfToken)
 app.use(flash())
 
 app.use((req, res, next) => {
-    if(!req.session.user) return next()
+    if (!req.session.user) return next()
     User.findById(req.session.user._id)
-        .then(user => {
+        .then((user) => {
+            throw new Error('Dummy Error')
+            if (!user) return next()
             req.user = user
             next()
         })
-        .catch(err => console.log('user failure from db', err))
+        .catch((err) => {
+            const error = new Error(err)
+            error.httpStatus = 500
+            next(error)
+        })
 })
 
 app.use((req, res, next) => {
-    
+
     res.locals.isAuthenticated = req.session.isLoggedIn,
-    res.locals.csrfToken =  req.csrfToken()
+        res.locals.csrfToken = req.csrfToken()
     next()
 })
 
-app.use(shopRoutes);
-app.use('/admin', adminRoutes);
-app.use(authRoutes);
 
-app.use(errorController.get404);
+app.use(shopRoutes)
+app.use('/admin', adminRoutes)
+app.use(authRoutes)
 
+app.use(errorController.get404)
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => {
+app.use((err, req, res, next) => {
+    if (err) {
+        res.status(500).render('500', {
+            pageTitle: 'Server Error',
+            path: '/500',
+        })
+    }
+})
+mongoose
+    .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then((result) => {
         console.log('connected to the client')
 
         app.listen(3030, () => {
             console.log('Listening on 3030')
         })
-    }).catch(err => console.log(err))
-
-
+    })
+    .catch((err) => console.log(err))
