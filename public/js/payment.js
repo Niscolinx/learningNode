d449df0f5337d584406b9b4eeaba5e15272f2d99
Igboanugwo Collins <munisco12@gmail.com>
2020-08-22 17:00:18 +0100

    Adding payment

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,13 +1,12 @@
-const paymentForm = document.getElementById('paymentForm')
-paymentForm.addEventListener('submit', payWithPaystack, false)
-function payWithPaystack(e) {
-    e.preventDefault()
+// const paymentForm = document.getElementById('paymentForm')
+// paymentForm.addEventListener('submit', payWithPaystack, false)
+const payWithPaystack = () =>  {
     let handler = PaystackPop.setup({
         key: 'pk_test_c5f057dc668f71b52b865b9529b412105f0135a2', // Replace with your public key
-        email: document.getElementById('email-address').value,
-        amount: document.getElementById('amount').value * 100,
-        firstname: document.getElementById('first-name').value,
-        lastname: document.getElementById('first-name').value,
+        email: 'test@test.com',
+        amount: 100,
+        firstname: 'Collins',
+        lastname: 'Munachi',
        
         onClose: function () {
             alert('Window closed.')
